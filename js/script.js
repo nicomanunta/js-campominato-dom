8ff72e4fcd9c7d24f8544856afce4a84c06810dd
nicomanunta <nic.manun@gmail.com>
2023-11-30 14:57:42 +0100

    Aggiunta definizione funzione per generare bombe

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+// Definizione funzione per creare le celle graficamente
 function createCell(num){
     let element = document.createElement("div")
     element.classList.add("square")
@@ -6,12 +6,15 @@ function createCell(num){
     
     return element
 }
+// Definizione funzione per far iniziare la partita
 function createGrid(){
     document.getElementById("grid").innerHTML = "";
     
     for(let i = 0; i<100; i++){
-        
+
+        // Chiamo la funzione che crea le celle graficamente
         let square = createCell(i+1)
+        // Le celle cambaino colore al click
         square.addEventListener("click", function(){
             this.classList.toggle("clicked")
             console.log("Hai cliccato la cella numero" +" "+ parseInt(i+1))
@@ -22,9 +25,17 @@ function createGrid(){
     
 }
 
-
-
-
+// Recupero il pulsante dal DOM e aggiungo il comando al click che chiama la funzione per far iniziare la partita
 document.getElementById("play").addEventListener("click", function(){
     createGrid()
 })
+
+
+// Definizione funzione che genera le bombe
+function generateBombs(number_bombs){
+    let bombs = []
+
+    for(let i = 0; i<number_bombs; i++){
+
+    }
+}
