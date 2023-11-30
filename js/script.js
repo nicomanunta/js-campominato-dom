a306b5b50c56a6a3fbf236895a0bafbbaa190587
nicomanunta <nic.manun@gmail.com>
2023-11-30 14:40:09 +0100

    Aggiunti file html css e js dall'esercizio di ieri

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+function createCell(num){
+    let element = document.createElement("div")
+    element.classList.add("square")
+    element.innerText = num
+    
+    return element
+}
+function createGrid(){
+    document.getElementById("grid").innerHTML = "";
+    
+    for(let i = 0; i<100; i++){
+        
+        let square = createCell(i+1)
+        square.addEventListener("click", function(){
+            this.classList.toggle("clicked")
+            console.log("Hai cliccato la cella numero" +" "+ parseInt(i+1))
+        })
+        grid.appendChild(square)
+    
+    }
+    
+}
+
+
+
+
+document.getElementById("play").addEventListener("click", function(){
+    createGrid()
+})
