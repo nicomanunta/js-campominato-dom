e32e705867fc674fd34a09b465d0db2490152275
nicomanunta <nic.manun@gmail.com>
2023-11-30 15:36:19 +0100

    aggiunto cambio di colore se si "clicca" una bomba

--- js/script.js
+++ js/script.js
@@ -1,36 +1,3 @@
-// Definizione funzione per creare le celle graficamente
-function createCell(num){
-    let element = document.createElement("div")
-    element.classList.add("square")
-    element.innerText = num
-    
-    return element
-}
-// Definizione funzione per far iniziare la partita
-function createGrid(){
-    document.getElementById("grid").innerHTML = "";
-    let number_bombs = 16
-    
-    for(let i = 0; i<100; i++){
-
-        // Chiamo la funzione che crea le celle graficamente
-        let square = createCell(i+1)
-        // Le celle cambaino colore al click
-        square.addEventListener("click", function(){
-            this.classList.toggle("clicked")
-            console.log("Hai cliccato la cella numero" +" "+ parseInt(i+1))
-        })
-        grid.appendChild(square)
-    
-    }
-    let bombs_generator = generateBombs(number_bombs)
-}
-
-// Recupero il pulsante dal DOM e aggiungo il comando al click che chiama la funzione per far iniziare la partita
-document.getElementById("play").addEventListener("click", function(){
-    createGrid()
-})
-
 // Definire una funzione che generi dei numeri casuali tutti diversi, se genera un numero gia generato non lo insrisce e ne genera un altro
 function generateRandomNumber(bombs_in_array){
     let check_number = false
@@ -53,6 +20,46 @@ function generateBombs(number_bombs){
         let num_bombs_in_array = generateRandomNumber(bombs)
         bombs.push(num_bombs_in_array)
     }
-
     console.log(bombs)
+    return bombs
 }
+
+
+// Definizione funzione per creare le celle graficamente
+function createCell(num){
+    let element = document.createElement("div")
+    element.classList.add("square")
+    element.innerText = num
+    
+    return element
+}
+// Definizione funzione per far iniziare la partita
+function createGrid(){
+    let number_bombs = 16
+    document.getElementById("grid").innerHTML = "";
+
+    let bombs_generator = generateBombs(number_bombs)
+
+    for(let i = 1; i<=100; i++){
+
+        // Chiamo la funzione che crea le celle graficamente
+        let square = createCell(i)
+        // Le celle cambiano colore al click
+        square.addEventListener("click", function(){
+            if(bombs_generator.includes(i) == false){
+                this.classList.add("clicked")
+                console.log("Hai cliccato la cella numero" +" "+ parseInt(i+1))
+            }
+            else{
+                this.classList.add("clicked-bomb")
+            }
+        })
+        grid.appendChild(square)
+    
+    }
+}
+
+// Recupero il pulsante dal DOM e aggiungo il comando al click che chiama la funzione per far iniziare la partita
+document.getElementById("play").addEventListener("click", function(){
+    createGrid()
+})
